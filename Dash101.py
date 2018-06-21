8409baef8e3dd5d23b38a6e940c20324aecf3dc8
ranga11 <vranga.11@gmail.com>
2018-06-20 20:02:24 -0700

    v1

--- Dash101.py
+++ Dash101.py
@@ -1,24 +1,27 @@
+#importing packages
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
 app = dash.Dash()
 
+#layout part where we define the html components
 app.layout = html.Div(children=[
     html.H1(children='Dash101'),
      dcc.Graph(
         id='example',
         figure={
             'data': [
-                {'x': [1, 2, 3, 4, 5], 'y': [9, 6, 2, 1, 5], 'type': 'line', 'name': 'Boats'},
-                {'x': [1, 2, 3, 4, 5], 'y': [8, 7, 2, 7, 3], 'type': 'bar', 'name': 'Cars'},
+                {'x': [1, 2, 3, 4, 5], 'y': [9, 5, 7, 3, 8], 'type': 'line', 'name': 'Boats'},
+                {'x': [1, 2, 3, 4, 5], 'y': [1, 9, 6, 2, 4], 'type': 'bar', 'name': 'Cars'},
             ],
             'layout': {
-                'title': 'Basic Sample'
+                'title': 'Dash Sample'
             }
         }
     )
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
